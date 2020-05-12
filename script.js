385ef16d52031536c3d0e6f83dd9cf97fcbba9af
greean <aj.green07@outlook.com>
2020-05-12 23:54:04 +0100

    Player 1 version nearly complete

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 // variables
+const image = document.getElementById("image");
+const button = document.getElementById("submit");
+const score = document.getElementById("score");
+const comment = document.getElementById("comment");
+let tally = 0;
 
+// main content
+submit.addEventListener("click", () => {
+    let rolled = randNum();
+    console.log(rolled);
+    for(let i = 1; i < 7; i++){
+        if(rolled == 1){
+            image.src = `img/dice1.png`;
+            comment.textContent = "Sorry! Start again!";
+            tally = 0;
+            score.textContent = tally;
+        }else if(rolled == i){
+            image.src = `img/dice${i}.png`;
+            comment.textContent = " ";
+            tally += rolled;
+            score.textContent = tally;
+        }
+    }
+})
+const randNum = () => {
+    return (Math.floor(Math.random() * 6) + 1);
+}
+//console.log(randNum);
+
+// Code for genearting a random number 1-6
+// let numList = [];
+
+// for(let i = 0; i < 6; i++){
+//     numList.push(Math.floor(Math.random() * 5) + 1);
+// }
+
+// console.log(numList);
