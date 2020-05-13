894f39a380144b8cf28c835a1c21043c6cf43bae
greean <aj.green07@outlook.com>
2020-05-13 16:01:17 +0100

    Player 1 game finished (basic styling)!

--- script.js
+++ script.js
@@ -4,22 +4,47 @@ const button = document.getElementById("submit");
 const score = document.getElementById("score");
 const comment = document.getElementById("comment");
 let tally = 0;
+let startAgain = false;
 
 // main content
+
+// checks if score needs to be reset after a win or loss condition
+const resetScore = () => {
+    if(startAgain == true){
+        comment.textContent = " ";
+        tally = 0;
+        score.textContent = tally;
+        startAgain = false;
+    }
+}
+// checks for winning condition after button clicked and rolled 2-6
+const checkWin = () => {
+    if(score.textContent > 19){
+        comment.textContent = "You win!!";
+        startAgain = true;
+        submit.textContent = "Start";
+    }else{
+        submit.textContent = "Roll";
+    }
+}
+
+
 submit.addEventListener("click", () => {
+    resetScore();
     let rolled = randNum();
     console.log(rolled);
     for(let i = 1; i < 7; i++){
         if(rolled == 1){
             image.src = `img/dice1.png`;
             comment.textContent = "Sorry! Start again!";
-            tally = 0;
-            score.textContent = tally;
+            startAgain = true;
+            submit.textContent = "Start";
         }else if(rolled == i){
             image.src = `img/dice${i}.png`;
             comment.textContent = " ";
             tally += rolled;
             score.textContent = tally;
+            checkWin();
         }
     }
 })
